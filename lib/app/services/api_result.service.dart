cddf1ac993654edb4a5656577addd25cec0aa8a8
Kitokoo <ezechiel@tajiri.io>
2024-03-12 16:22:49 +0000

    add api result service

---
+++ lib/app/services/api_result.service.dart
@@ -0,0 +1,28 @@
+// import 'network_exceptions.dart';
+
+class ApiResultService<T> {
+  // final T? data;
+  // final NetworkExceptions error;
+  // final int statusCode;
+
+  // const ApiResultService.success({required this.data})
+  //     : error = const NetworkExceptions(),
+  //       statusCode = 200;
+
+  // const ApiResultService.failure(
+  //     {required this.error, required this.statusCode})
+  //     : data = null;
+
+  // R when<R>({
+  //   required R Function(T? data) success,
+  //   required R Function(NetworkExceptions error, int statusCode) failure,
+  // }) {
+  //   if (this.data != null) {
+  //     return success(this.data);
+  //   } else if (this.error != null && this.statusCode != null) {
+  //     return failure(this.error, this.statusCode);
+  //   } else {
+  //     throw Exception('Invalid ApiResult type');
+  //   }
+  // }
+}
