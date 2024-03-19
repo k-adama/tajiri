e1bfac509a8436db006da0a792ab471c80e6f010
Kitokoo <ezechiel@tajiri.io>
2024-03-19 16:56:06 +0000

    home screen get user data

--- lib/presentation/screens/navigation/home/components/shop_bar_item.component.dart
+++ lib/presentation/screens/navigation/home/components/shop_bar_item.component.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:tajiri_pos_mobile/app/config/theme/style.theme.dart';
 import 'package:tajiri_pos_mobile/app/mixpanel/mixpanel.dart';
 import 'package:tajiri_pos_mobile/domain/entities/story.entity.dart';
 import 'package:tajiri_pos_mobile/presentation/controllers/navigation/home/home.controller.dart';
 import 'package:tajiri_pos_mobile/presentation/screens/navigation/home/components/shop_avatar.component.dart';
+import 'package:tajiri_pos_mobile/presentation/screens/navigation/home/story.screen.dart';
 
 class ShopBarItemComponent extends StatelessWidget {
   final RefreshController controller;
@@ -29,10 +31,12 @@ class ShopBarItemComponent extends StatelessWidget {
         Mixpanel.instance.track("Status viewed",
             properties: {"Canal viewed": story?.title, "Status": "Succes"});
         // Get.toNamed(Routes.STORY_PAGE);
+        Get.to(const StoryScreen());
       },
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
               decoration: BoxDecoration(
