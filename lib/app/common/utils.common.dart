f085f97275fd5b53855bab6694f56459d67fc5f3
kindo-adama <146742695+kindo-adama@users.noreply.github.com>
2024-05-24 12:03:49 +0000

    Update utils.common.dart

--- lib/app/common/utils.common.dart
+++ lib/app/common/utils.common.dart
@@ -36,25 +36,10 @@ ListingType? checkListingType(UserEntity? user) {
 }
 
 String userOrWaitressName(OrderEntity orderItem, UserEntity? user) {
-  final createdUserName = "${user?.firstname ?? ""} ${user?.lastname ?? ""}";
+  final createdUserName = "${orderItem.createdUser?.firstname ?? ""} ${orderItem.createdUser?.lastname ?? ""}";
   final createdUserOrtableName = orderItem.tableId != null ? orderItem.table?.name ?? createdUserName : "-----";
   return checkListingType(user) == ListingType.waitress ? (orderItem.waitressId != null ? orderItem.waitress?.name ?? createdUserOrtableName : createdUserOrtableName) : createdUserOrtableName;
 }
-
-/*String userOrWaitressName(OrderEntity orderItem, UserEntity? user) {
-  final creataedUserName =
-      "${orderItem.createdUser?.firstname ?? ""} ${orderItem.createdUser?.lastname ?? ""}";
-  final createdUserOrtableName = orderItem.tableId != null
-      ? orderItem.table?.name ?? creataedUserName
-      : "-----";
-  if (checkListingType(user) == ListingType.waitress) {
-    return orderItem.waitressId != null
-        ? orderItem.waitress?.name ?? createdUserOrtableName
-        : createdUserOrtableName;
-  } else {
-    return createdUserOrtableName;
-  }
-}*/
 
 getInitialName(String fullName) {
   List<String> nameParts = fullName.split(" ");
