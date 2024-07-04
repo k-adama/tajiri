dc57e18d702d7b1630932f55b0083c724ae399d7
Kitokoo <ezechiel@tajiri.io>
2024-07-04 12:51:20 +0000

    choose invoice size

--- lib/presentation/screens/bluetooth_setting/bluetooth_setting.screen.dart
+++ lib/presentation/screens/bluetooth_setting/bluetooth_setting.screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:tajiri_pos_mobile/app/config/theme/style.theme.dart';
 import 'package:tajiri_pos_mobile/presentation/controllers/navigation/navigation.controller.dart';
